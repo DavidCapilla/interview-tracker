2d02f814672b8ec1315fd3699a9634c5e43ee610
AnaG0307 <ana.genover@gmail.com>
2026-09-01 20:57:14 +0200

    Created fake data and created first class to retrieve data

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from dataclasses import dataclass
+from datetime import datetime
 
 app = FastAPI()
 
@@ -6,3 +8,37 @@ app = FastAPI()
 @app.get("/health")
 def read_health() -> dict[str, str]:
     return {"status": "ok"}
+
+
+# @app.get("/exercise")
+# def get_exercise():
+#     return Exercise("push ups", 5, 3, 10).volume()
+#
+#
+# @dataclass
+# class Exercise:
+#     name: str
+#     reps: int
+#     sets: int
+#     weight: float
+#
+#     def volume(self) -> float | int :
+#         return self.reps * self.weight
+
+
+@dataclass
+class Application:
+    id: str
+    company_name: str
+    date: datetime
+    position: str
+
+
+@app.get("/applications")
+def read_applications() -> list[Application]:
+    return [Application("45", "Google", datetime(2025, 3, 1, 14, 30), "back end"),
+    Application("55", "Amazon", datetime(2025, 3, 1, 14, 35), "fullstack")]
+
+# datetime(2025, 3, 1, 14, 30)
+
+
